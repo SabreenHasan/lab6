7ea5aa4e257f3bb1c8ec60e8bdd2b1d8059c3a80
Sabreen Hasan <sabreen_hasan@yahoo.com>
2018-11-13 22:49:32 -0500

    Completed exercise 2

---
+++ search.js
@@ -0,0 +1,32 @@
+// JavaScript File
+
+window.onload = function () {
+    var httpReq;
+    var searchB = document.getElementById("searchButton");
+    
+    searchB.addEventListener('click',function(e) {
+
+    e.preventDefault();
+    httpReq = new XMLHttpRequest();
+    
+    var url = "request.php?q=definition";
+    httpReq.onreadystatechange = load;
+    httpReq.open('GET', url);
+    httpReq.send();
+});
+
+function load () {
+    if  (httpReq.readyState === XMLHttpRequest.DONE)
+    {
+        if (httpReq.status === 200)
+        {
+            var response = httpReq.responseText;
+            alert(response);
+        }
+        else
+        {
+            alert('ERROR!');
+        }
+    }
+}
+};
